5bf925d8827fba02ac0687e925197db23d4a336e
TalZ <talzana55@gmail.com>
2022-05-09 11:44:10 +0200

    exemple sur internet

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+#Probleme du sac à dos a 2 sac
+#Auteurs: Tal ZANA | Benjamin KRIEF
+#Date: 09-05-2022
+#Version: 1.0
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class ObjetSac:
+    def __init__(self, poids, valeur, indice):
+        self.indice = indice
+        self.poids = poids
+        self.valeur = valeur
+        self.rapport = valeur // poids
+        # Fonction pour la comparaison entre deux ObjetSac
+
+    # On compare le rapport calculé pour les trier
+    def __lt__(self, other):
+        return self.rapport < other.rapport
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def getValeurMax(poids, valeurs, capacite):
+    tableauTrie = []
+    for i in range(len(poids)):
+        tableauTrie.append(ObjetSac(poids[i], valeurs[i], i))
+
+        # Trier les éléments du sac par leur rapport
+    tableauTrie.sort(reverse=True)
+
+    compteurValeur = 0
+    for objet in tableauTrie:
+        poidsCourant = int(objet.poids)
+        valeurCourante = int(objet.valeur)
+        if capacite - poidsCourant >= 0:
+            # on ajoute l'objet dans le sac
+            # On soustrait la capacité
+            capacite -= poidsCourant
+            compteurValeur += valeurCourante
+            # On ajoute la valeur dans le sac
+    return compteurValeur
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+poids = [1, 5, 3, 2, 4]
+valeurs = [10, 50, 20, 30, 60]
+capacite = 11
+valeurMax = getValeurMax(poids, valeurs, capacite)
+print("Valeur maxi dans le sac à dos =", valeurMax)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
